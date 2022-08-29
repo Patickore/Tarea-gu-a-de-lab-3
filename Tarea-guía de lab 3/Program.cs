73f3d60e77b4743ff4a2eff77cdca11ba7e8972a
Patrick <patri@LAPTOP-Q3T1BCTA>
2022-08-29 17:18:36 -0600

    Agregar archivos de proyecto.

---
+++ Tarea-guía de lab 3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tarea_guía_de_lab_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
