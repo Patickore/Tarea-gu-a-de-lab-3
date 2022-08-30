36ba3a9002cfad785c8c522f22805c78308f8317
Patrick <patri@LAPTOP-Q3T1BCTA>
2022-08-29 18:18:52 -0600

    Actualizacion

---
+++ App3/List.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace App3
+{
+    internal class List<T>
+    {
+        public int Count { get; internal set; }
+        public int Capacity { get; internal set; }
+
+        internal void Add(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void RemoveRange(int v1, int v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Insert(int v1, string v2)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void Clear()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ App3/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace App3
+{
+    class Program
+    {
+        private static bool row;
+
+        static void Main(string[] args)
+        {
+            List<string> lista = new List<string>();
+            Console.WriteLine("Cantidad: {0}", lista.Count);
+            Console.WriteLine("Capacidad: {0}", lista.Capacity);
+            Console.WriteLine();
+
+            for (int i = 0; i < 10; i++)
+            {
+                lista.Add("Dato " + i);
+            }
+            Console.WriteLine("Cantidad: {0}", lista.Count);
+            Console.WriteLine("Capacidad: {0}", lista.Capacity);
+            Console.WriteLine();
+
+            for (int i = 10; i < 33; i++)
+            {
+                lista.Add("Dato " + i);
+            }
+            Console.WriteLine("Cantidad: {0}", lista.Count);
+            Console.WriteLine("Capacidad: {0}", lista.Capacity);
+            Console.WriteLine();
+
+            foreach (string row in lista) 
+            {
+                Console.WriteLine(row);
+            }
+
+            Console.WriteLine(" ========== Eliminar rango ==========");
+            lista.RemoveRange(5, 4);
+
+            foreach (string row in lista) 
+            {
+                Console.WriteLine(row);
+            }
+
+            lista.Insert(11, "Elemento nuevo");
+
+            foreach (string row in lista) 
+            {
+                Console.WriteLine(row);
+            }
+            
+            lista.Clear();
+            Console.WriteLine("Cantidad: {0}", lista.Count);
+            Console.WriteLine("Capacidad: {0}", lista.Capacity);
+        }
+    }
+}
