4deeec66dbd1270c247808c3b1706c4fb09123a6
Patrick <patri@LAPTOP-Q3T1BCTA>
2022-08-29 18:33:49 -0600

    Actualizacion

---
+++ App4/Hashtable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace App4
+{
+    internal class Hashtable
+    {
+        public bool Count { get; internal set; }
+        public IEnumerable<int> Keys { get; internal set; }
+
+        internal void Add(int i, string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ App4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace App4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable lista = new Hashtable();
+            Console.WriteLine(lista.Count);
+
+            for (int i = 0; i < 10; i++)
+            {
+                lista.Add(i, "Dato " + i);
+            }
+
+            foreach (int key in lista.Keys)
+            {
+                Console.WriteLine("Clave: {0} === Valor: {1}", key, lista[key]);
+            }
+        }
+    }
+}
